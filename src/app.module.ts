57f7d0b6a6d835b6e2f3ef698bcb537ce484b4d5
felipe <feleteli@egresados.ubiobio.cl>
2022-06-10 23:50:04 -0400

    :sparkles: import modules in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { RewardsModule } from './rewards/rewards.module';
 import { CredentialsModule } from './credentials/credentials.module';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TasksModule } from './tasks/tasks.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [RewardsModule, CredentialsModule,ConfigModule.forRoot()],
+  imports: [RewardsModule, CredentialsModule, ConfigModule.forRoot(), ScheduleModule.forRoot(), TasksModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
