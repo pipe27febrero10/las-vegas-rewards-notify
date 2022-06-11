9132cd4d4a9f6c6cf069386a84484bc811a787cc
felipe <feleteli@egresados.ubiobio.cl>
2022-06-10 23:49:00 -0400

    :sparkles: modify  rewards ids

---
+++ src/rewards/constants/configuration.ts
@@ -0,0 +1,13 @@
+export enum RewardsPrizes{
+    TOURNAMENT_OF_KINGS = 24505,
+    MANDALAY_BAY_ACUARIUM = 24555,
+    MADAME_TUSSAUDS_MUSEUM = 24737,
+    HALF_PRICE_BUFFET_BELLAGIO = 25201
+}
+
+//24505
+// 25201
+//estan disponibles
+//  las otras salen como undefined
+// 25282  maddame tsusauds
+
